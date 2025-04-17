6001c8776043d5379b7bc9f56090946cc1a97695
OussamaZermouq <oussamazermouq2@gmail.com>
2025-04-17 09:54:21 +0100

    Ceated a new branch Oussama

--- src/components/dashboard/components/MainGrid.js
+++ src/components/dashboard/components/MainGrid.js
@@ -25,6 +25,7 @@ export default function MainGrid({
   cellAvailabilityProp,
   sessionContinuityProp,
 }) {
+
   const [rrcConnectionRate, setRrcConnectionRate] = useState(
     rrcConnectionRateProp
   );
